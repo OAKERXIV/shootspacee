7da0b42d7f7422bdec33b33b407449a18e91430f
OAKERXIV <144644661+OAKERXIV@users.noreply.github.com>
2023-09-10 20:25:44 -0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1694365688,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/bg.png",
+		"images/player-sheet0.png",
+		"images/thrush-sheet0.png",
+		"images/thrush-sheet1.png",
+		"images/bullet-sheet0.png",
+		"images/rock-sheet0.png",
+		"images/particles.png",
+		"images/spawner-sheet0.png",
+		"media/savetube.app-laser-gun-sound-effect-free-_128-kbps_.ogg",
+		"media/savetube.app-space-laser-sound-effect-_128-kbps__1 (1).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
